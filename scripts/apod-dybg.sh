baeb011237bd41473ea471016d3a8092ebde82fb
boot1110001 <ppuubblliicc@protonmail.com>
2018-12-05 23:21:22 +0100

    organización buena 1 + primer paso de añadir apod2 por si acaso

--- scripts/apod-dybg.sh
+++ scripts/apod-dybg.sh
@@ -20,8 +20,8 @@ DESCRIPTION_DIR="$HOME/.apod-dybg/bg-description"
 
 # Enter 'Length' in the language of your PC bash shell.
 #~ LENGTH="Length"
-#~ LENGTH="Longitud"
-LENGTH="Lonxitude"
+LENGTH="Longitud"
+#~ LENGTH="Lonxitude"
 
 # Address of the default image.
 #~ DEFAULT_IMG="$HOME/.apod-dybg/bg-default/bg-default-"$(( ( RANDOM % 3 )  + 1 ))".jpg"
@@ -38,10 +38,29 @@ TODAY=$(date +'%Y%m%d')
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # 	FUNCTIONS
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+function intro {
+	echo " ==========================="
+	echo " ==    Wallpaper APOD     =="
+	echo " ==========================="
+	echo "       by boot1110001       "
+	echo " ==========================="
+	echo "   Inspired by a script of  "
+	echo "    Josh Schreuder (2011)   "
+	echo " ==========================="
+	echo "  GIT https://goo.gl/yPSXjL "
+	echo ""
+}
+
 function get_page {
     echo "Downloading the page to find the image..."
     wget http://apod.nasa.gov/apod/ --quiet -O /tmp/apod.html
     grep -m 1 jpg /tmp/apod.html | sed -e 's/<//' -e 's/>//' -e 's/.*=//' -e 's/"//g' -e 's/^/http:\/\/apod.nasa.gov\/apod\//' > /tmp/pic_url
+}
+
+function get_page3 {
+    echo "Downloading the page 2 to find the image..."
+    wget http://www.aapodx2.com/ --quiet -O /tmp/apod.html
+    grep -m 2 jpg /tmp/apod.html | sed -n '1!p' | sed -e 's/.*href="//' -e 's/><img.*//' -e 's/"//g' -e 's/^/http:\/\/www.aapodx2.com/' > /tmp/pic_url3
 }
 
 function save_description {
@@ -78,7 +97,6 @@ function clean_up {
     if [ -e "/tmp/apod.html" ]; then
         rm /tmp/apod.html
     fi
-    
 }
 
 function check {
@@ -99,16 +117,7 @@ function check {
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # 	START
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-echo " ==========================="
-echo " ==    Wallpaper APOD     =="
-echo " ==========================="
-echo "       by boot1110001       "
-echo " ==========================="
-echo "   Inspired by a script of  "
-echo "    Josh Schreuder (2011)   "
-echo " ==========================="
-echo "  GIT https://goo.gl/yPSXjL "
-echo ""
+intro
 
 echo "The path of the new file to save is: $PICTURES_DIR/"${TODAY}"_apod.jpg"
 
@@ -131,8 +140,6 @@ if [[ "$PICURL" =~ ^(http:\/\/apod.nasa.gov\/.*)(\.jpg|\.png)$ ]];
 		# If we do not have today's image yet.
 		if [ ! -e "$PICTURES_DIR/"${TODAY}"_apod.jpg" ]; then
 			echo "The image is not saved, saving it..."
-			
-			#~ echo "The URL of the image is: ${PICURL}"
 
 			echo "Downloading image ..."
 			wget --quiet $PICURL -O $PICTURES_DIR/${TODAY}_apod.jpg
@@ -145,12 +152,10 @@ if [[ "$PICURL" =~ ^(http:\/\/apod.nasa.gov\/.*)(\.jpg|\.png)$ ]];
 			echo "Cleaning the previous image..."
 			# Deleting the previous image (everything that is not today's image)
 			rm $PICTURES_DIR/$(ls $PICTURES_DIR | grep -v ${TODAY}_apod.jpg)
-
 		else
 		# If we already have the image, check that it is the most updated copy.
 
 			echo "We already have an image, checking that it is the most updated copy..."
-			#~ echo "The URL of the image is: ${PICURL}"
 
 			# Getting the filesize (image size).
 			SITEFILESIZE=$(wget --spider $PICURL 2>&1 | grep $LENGTH | awk '{print $2}')
@@ -163,10 +168,6 @@ if [[ "$PICURL" =~ ^(http:\/\/apod.nasa.gov\/.*)(\.jpg|\.png)$ ]];
 				echo "The image has not been updated (the sizes don't match), obtaining the updated copy..."
 				rm $PICTURES_DIR/${TODAY}_apod.jpg
 
-				# Getting the link of the image.
-				#~ PICURL=`/bin/cat /tmp/pic_url`
-				#~ echo "The URL of the image is: ${PICURL}"
-
 				echo "Downloading image ..."
 				wget --quiet $PICURL -O $PICTURES_DIR/${TODAY}_apod.jpg
 
@@ -174,7 +175,6 @@ if [[ "$PICURL" =~ ^(http:\/\/apod.nasa.gov\/.*)(\.jpg|\.png)$ ]];
 				gsettings set org.gnome.desktop.background picture-uri "file://"$PICTURES_DIR/${TODAY}_apod.jpg
 				
 				save_description
-
 			else
 			# If the image has been updated (the sizes match).
 				echo "The image has already been updated (the sizes match), finishing."
@@ -184,17 +184,17 @@ if [[ "$PICURL" =~ ^(http:\/\/apod.nasa.gov\/.*)(\.jpg|\.png)$ ]];
 			fi
 		fi
 		
+		clean_up
+		check
+		
 	else 
 		echo "No, it's not an image (the URL does not have a correct format) :(" 
 		echo "The URL is not a downloadable image, replacing it with the default image..."
 		echo "Default image: $DEFAULT_IMG"
+		
 		gsettings set org.gnome.desktop.background picture-uri "file://$DEFAULT_IMG"
 		notify-send -i "$ICON" "Fallo en la descarga del fondo de pantalla APOD" "La imagen de hoy de la 'Astronomy Picture of the Day' no se ha podido descargar. Asignado el fondo por defecto."
 		clean_up
-		exit
 fi
 
-
-
-clean_up
-check
+exit
